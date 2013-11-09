37017330aee947cb892599f82381c2dbd55513a1
Egor Day <edaydev@gmail.com>
2013-11-09 10:47:17 +0200

    Adapted to absense of content_shell in new Dartium distrib

---
+++ tool/install_cs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+which content_shell
+if [[ $? -ne 0 ]]; then
+  $DART_SDK/../chromium/download_contentshell.sh
+  unzip content_shell-linux-x64-release.zip
+
+  cs_path=$(ls -d drt-*)
+  export PATH=$cs_path:$PATH
+fi
